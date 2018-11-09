13bda5014136f656d47ba574e13f033e2e633ff2
Gustavo Chagas <gustavochagasb@gmail.com>
2018-11-09 13:15:26 -0200

    Integration with API

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
+import api from './services/api';
 
 import Header from './components/Header';
+import Main from './pages/main';
 
 import './styles.css';
 
 const App = () => (
   <div className="App">
     <Header />
+    <Main />
   </div>
 )
 
